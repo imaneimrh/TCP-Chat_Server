a7dcf231ecf8d090724328d5213b095c10e29510
Khaoula Mej <khaoula.mejhoudi@gmail.com>
2025-02-24 22:22:34 +0000

    Added client connection management

--- server/server.go
+++ server/server.go
@@ -1,17 +1,31 @@
 package server
 
 import (
+	"bufio"
 	"log"
 	"net"
+	"sync"
 )
 
 type Server struct {
-	Addr string
+	Addr         string
+	Clients      map[string]*Client
+	ClientsMutex sync.RWMutex
+}
+
+type Client struct {
+	ID            string
+	Conn          net.Conn
+	Writer        *bufio.Writer
+	Server        *Server
+	Username      string
+	Authenticated bool
 }
 
 func NewServer(addr string) *Server {
 	return &Server{
-		Addr: addr,
+		Addr:    addr,
+		Clients: make(map[string]*Client),
 	}
 }
 
@@ -35,25 +49,72 @@ func (s *Server) Start() error {
 }
 
 func (s *Server) handleConnection(conn net.Conn) {
-	clientAddr := conn.RemoteAddr().String()
-	log.Printf("New connection from %s", clientAddr)
+	clientID := conn.RemoteAddr().String()
+	client := &Client{
+		ID:            clientID,
+		Conn:          conn,
+		Writer:        bufio.NewWriter(conn),
+		Server:        s,
+		Authenticated: false,
+	}
+	s.registerClient(client)
+	client.Send("Welcome to the TCP Chat Server!")
+	scanner := bufio.NewScanner(conn)
 
-	conn.Write([]byte("Welcome to the TCP Chat Server!\n"))
+	for scanner.Scan() {
+		message := scanner.Text()
 
-	buffer := make([]byte, 1024)
+		log.Printf("Received from %s: %s", client.ID, message)
 
-	for {
-		n, err := conn.Read(buffer)
-		if err != nil {
-			log.Printf("Client %s disconnected: %v", clientAddr, err)
-			break
-		}
-
-		message := string(buffer[:n])
-		log.Printf("Received from %s: %s", clientAddr, message)
-		conn.Write([]byte("Echo: " + message))
+		client.Send("Echo: " + message)
 	}
 
-	conn.Close()
-	log.Printf("Connection closed: %s", clientAddr)
+	if err := scanner.Err(); err != nil {
+		log.Printf("Error reading from client %s: %v", client.ID, err)
+	}
+
+	s.removeClient(client)
+}
+
+func (s *Server) registerClient(client *Client) {
+	s.ClientsMutex.Lock()
+	defer s.ClientsMutex.Unlock()
+
+	s.Clients[client.ID] = client
+	log.Printf("New client connected: %s", client.ID)
+}
+
+func (s *Server) removeClient(client *Client) {
+	s.ClientsMutex.Lock()
+	defer s.ClientsMutex.Unlock()
+
+	if _, exists := s.Clients[client.ID]; exists {
+		delete(s.Clients, client.ID)
+		client.Conn.Close()
+		log.Printf("Client disconnected: %s", client.ID)
+	}
+}
+
+func (s *Server) Broadcast(message string, sender *Client) {
+	s.ClientsMutex.RLock()
+	defer s.ClientsMutex.RUnlock()
+
+	for _, client := range s.Clients {
+		if client.ID != sender.ID {
+			client.Send(message)
+		}
+	}
+}
+
+func (c *Client) Send(message string) {
+	_, err := c.Writer.WriteString(message + "\n")
+	if err != nil {
+		log.Printf("Error sending message to client %s: %v", c.ID, err)
+		return
+	}
+
+	err = c.Writer.Flush()
+	if err != nil {
+		log.Printf("Error flushing message to client %s: %v", c.ID, err)
+	}
 }
